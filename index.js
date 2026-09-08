c0f7d0fec932c66178cfea284ff0c57225d8cfcb
zidd. <fadillahyazid444@gmail.com>
2026-09-08 15:08:49 +0700

    Initialize Express server with routes and middleware

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import helmet from "helmet";
+import "dotenv/config";
+import watchlistRoutes from "./routes/watchlist.js";
+import authRoutes from "./routes/auth.js";
+
+
+const PORT = process.env.PORT;
+const app = express();
+
+app.use(helmet());
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+  res.send("Family Movie Watchlist API");
+});
+
+app.use("/api/watchlist", watchlistRoutes);
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}...`);
+});
